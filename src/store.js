c549cbd39952392628ab510e901d0ebb08116799
UvarovIV <107835250+UvarovIV@users.noreply.github.com>
2024-04-22 22:57:43 +0300

    Delete src directory

--- src/store.js
+++
@@ -1,7 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import lightConeReducer from "./slices/lightConeSlice";
-export default configureStore({
-    reducer: {
-        lightCones: lightConeReducer,
-    },
-})
